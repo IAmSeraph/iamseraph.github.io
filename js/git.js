cd22e14e9867969bb346ccd1fb8a04e5d6f2c503
IAmSeraph <5700945+IAmSeraph@users.noreply.github.com>
2023-07-04 20:43:36 +0200

    fixing some stuff, will continue later

--- js/git.js
+++ js/git.js
@@ -1,20 +1,37 @@
-jQuery.githubUser = function(user, cbfun) { $.ajax({ dataType: "json", url: "https://api.github.com/users/" + user + "/repos?callback=?", headers: {  Authorization: "Bearer " + $("meta[name=\"csrf-token\"]").attr("content"), Accept: "application/vnd.github+json" }, success: cbfun, error: function(error) { console.log(error); } }); };
+jQuery.githubUser = function(user, cbfun) { $.ajax({ dataType: "json", url: "https://api.github.com/users/" + user + "/repos?callback=?", beforeSend: function(xhr) { xhr.setRequestHeader("authorization", "Bearer " + $("meta[name=csrf-token]").attr("content"));}, success: cbfun, error: $(".projects").html("<span style=\"color:white;font-size:10vh;text-align:center;width:70vw;\">Rate limit reached!</span>") }); };
 jQuery.loadRepositories = function(user) {
-    $(".projects").html("<h2>Querying GitHub for projects...</h2>");
+    $(".projects").html("<span style=\"color:white;font-size:10vh;text-align:center;float:right;width:70vw;\">Querying GitHub for projects...</span>");
     $.githubUser(user, function(repositories) {
-        $(".projects").empty();
+        console.log(repositories);
         let display = "";
         $(repositories.data).each(function() {
-            let project = "<div class=\"project\">\n<div class=\"heading\">\n<h3>" + this.name + "</h3>\n<div class=\"social\">\n<a href=\"" + this.html_url + "\" target=\"_blank\" rel=\"Link to GitHub\"><i class=\"fab fa-github\"></i></a>\n";
+            let project = "<div class=\"project\">\n"
+            project += "<div class=\"heading\">\n";
+            project += "<h3>" + this.name + "</h3>\n";
+            project += "<div class=\"social\">\n";
+            project += "<a href=\"" + this.html_url + "\" target=\"_blank\" rel=\"Link to GitHub\"><i class=\"fab fa-github\"></i></a>\n";
             if (this.homepage !== "" && this.homepage !== null)
                 project += "<a href=\"" + this.homepage + "\" target=\"_blank\" rel=\"Link to website\"><i class=\"fas fa-globe-europe\"></i></a>\n";
-            project += "</div>\n</div></br>\n<div class=\"content\">\n<p>" + this.description + "</p>\n</div></br>\n<div class=\"chips\">\n<span class=\"chip\">" + this.private ? "Private" : "Public" + "</span>\n</div></br>\n<div class=\"chips\">\n";
-            if (this.language !== "" && this.language !== null)
-                $(this.language).each(function(language) { project += "<span class=\"chip\">" + language + "</span>\n"; });
-            project += "</div></br>\n<div class=\"chips\">\n";
-            if (this.topics !== "" && this.topics !== null)
-                $(this.topics).each(function(topic) { project += "<span class=\"chip\">" + topic + "</span>\n"; });
-            project += "</div></br>\n</div>\n";
+            project += "</div>\n";
+            project += "</div></br>\n";
+            project += "<div class=\"content\">\n";
+            project += "<p>" + this.description + "</p>\n";
+            project += "</div></br>\n";
+            project += "<div class=\"chips\">\n<span class=\"chip\">"+(this.private?"Private":"Public")+"</span>\n</div></br>\n";
+            if (this.language != "" && this.language != null) {
+                console.log(this.language);
+                project += "<div class=\"chips\">\n";
+                let languages = this.language.split(" ");
+                for (let language of languages) project += "<span class=\"chip\">" + language + "</span>\n";
+                project += "</div></br>\n";
+            }
+            if (this.topics != "" && this.topics != null) {
+                console.log(this.topics);
+                project += "<div class=\"chips\">\n";
+                for (let topic of this.topics) project += "<span class=\"chip\">" + topic + "</span>\n";
+                project += "</div></br>\n";
+            }
+            project += "</div>\n";
             display += project;
         });
         $(".projects").html(display);
